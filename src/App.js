822791c7f31f9a43db7806203dc98d655624bae2
Duc <nguyenthanhduc242004@gmail.com>
2024-10-27 15:47:28 +0700

    Project base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Following from './pages/Following';
+import { DefaultLayout } from './components/Layouts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <DefaultLayout>
+                            <Home />
+                        </DefaultLayout>
+                    }
+                />
+                <Route
+                    path="/following"
+                    element={
+                        <DefaultLayout>
+                            <Following />
+                        </DefaultLayout>
+                    }
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
